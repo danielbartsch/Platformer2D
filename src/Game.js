f87aa196e307d863e08d8be6e807c92b3844155b
danielbartsch <daniel.bartsch@live.at>
2017-12-08 01:00:33 +0100

    put levels into files that can be loaded

--- src/Game.js
+++ src/Game.js
@@ -3,9 +3,8 @@
 import React, { Component } from 'react'
 import { map, some } from 'lodash'
 import { drawRelatively } from './camera'
-import { nextState, isInsideBounds, getDimensions } from './entityUtils'
-import * as EntityTypes from './entityTypes'
-import type { Entity, Bounds } from './flowTypes'
+import { nextState, isInsideBounds } from './entityUtils'
+import type { Bounds } from './flowTypes'
 import {
 	getGamePadButtonNames,
 	isLeftPressed,
@@ -20,85 +19,6 @@ import { renderMenu } from './menuUtils'
 import { setCanvasOptions } from './canvasUtils'
 import styles from './App.css'
 
-const backgroundEntities: Array<Entity> = [
-	{
-		type: EntityTypes.BACKGROUND,
-		...getDimensions(EntityTypes.BACKGROUND),
-		accelerationX: 0,
-		accelerationY: 0,
-		maxAccelerationX: 0,
-		maxAccelerationY: 0,
-		maxVelocityX: 0,
-		maxVelocityY: 0,
-		velocityX: 0,
-		velocityY: 0,
-		isObstacle: false,
-		isStanding: false,
-		x: -10,
-		y: -10,
-	},
-]
-
-/* eslint-disable max-len */
-const indestructibleEntities: Array<Entity> = [
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 0, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 50, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 100, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 150, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 200, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_2, ...getDimensions(EntityTypes.BLOCK_2), isObstacle: true, x: 250, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: 300, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: 200, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: 100, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: -100, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -110, y: 350, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -110, y: 250, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -110, y: 150, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -110, y: 50, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -110, y: -50, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 100, y: 380, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 120, y: 380, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 140, y: 380, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 140, y: 360, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 160, y: 340, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 290, y: 400, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 310, y: 420, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 330, y: 440, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 350, y: 460, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 370, y: 480, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 390, y: 500, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 410, y: 520, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 430, y: 540, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 450, y: 560, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 470, y: 580, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 490, y: 600, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-	{ type: EntityTypes.BLOCK_1, ...getDimensions(EntityTypes.BLOCK_1), isObstacle: true, x: 510, y: 620, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
-]
-/* eslint-enable */
-
-const destructibleEntities: Array<Entity> = []
-const enemyEntities: Array<Entity> = []
-const mainCharacter: Array<Entity> = [
-	{
-		type: EntityTypes.MAIN_CHARACTER,
-		...getDimensions(EntityTypes.MAIN_CHARACTER),
-		x: 30,
-		y: 300,
-		velocityX: 0,
-		velocityY: 0,
-		accelerationX: 0,
-		accelerationY: 0.9,
-		maxVelocityX: 3,
-		maxVelocityY: 12,
-		maxAccelerationX: 0.2,
-		maxAccelerationY: 0.9,
-		isStanding: false,
-		isObstacle: true,
-	},
-]
-const effectEntities: Array<Entity> = []
-const foreGroundEntities: Array<Entity> = []
 const menuEntities = []
 
 let cameraX = 0
@@ -106,12 +26,32 @@ let cameraY = 0
 
 let tick = 0
 
+let level = require('./levels/test').default()
+
 let isPaused = false
 
 const pauseMenuItems = [
-	{ text: 'Resume', style: 'BlanchedAlmond', font: '23px Lucida Console', onActivate: () => { isPaused = false } },
-	{ text: 'Levels', style: 'BlanchedAlmond', font: '23px Lucida Console', onActivate: () => {} }, // nothing just yet
-	{ text: 'Restart', style: 'BlanchedAlmond', font: '23px Lucida Console', onActivate: () => {} }, // reload level
+	{
+		text: 'Resume',
+		style: 'BlanchedAlmond',
+		font: '23px Lucida Console',
+		onActivate: () => { isPaused = false },
+	},
+	{
+		text: 'Levels',
+		style: 'BlanchedAlmond',
+		font: '23px Lucida Console',
+		onActivate: () => {}, // nothing just yet
+	},
+	{
+		text: 'Restart',
+		style: 'BlanchedAlmond',
+		font: '23px Lucida Console',
+		onActivate: () => {
+			isPaused = false
+			level = require('./levels/test').default() // eslint-disable-line global-require
+		},
+	},
 ]
 let selectedPauseMenuItemIndex = 0
 
@@ -154,8 +94,8 @@ export default class Game extends Component<Props> {
 	props: Props
 
 	getCameraBounds = (): Bounds => ({
-		x: (mainCharacter[0].x - (this.props.width / 2)) + cameraX,
-		y: (mainCharacter[0].y - (this.props.height / 2)) + cameraY,
+		x: (level.mainCharacter[0].x - (this.props.width / 2)) + cameraX,
+		y: (level.mainCharacter[0].y - (this.props.height / 2)) + cameraY,
 		width: this.props.width,
 		height: this.props.height,
 	})
@@ -163,16 +103,16 @@ export default class Game extends Component<Props> {
 	camera = () => {
 		if (!isPaused) {
 			if (isUpPressed(keys, gamePad)) {
-				cameraY = cameraY - 2 > (-this.props.height / 2) + mainCharacter[0].height ? cameraY - 2 : cameraY
+				cameraY = cameraY - 2 > (-this.props.height / 2) + level.mainCharacter[0].height ? cameraY - 2 : cameraY
 			} else if (isDownPressed(keys, gamePad)) {
 				cameraY = cameraY + 2 < (this.props.height / 2) ? cameraY + 2 : cameraY
 			} else {
 				cameraY *= 0.9
 			}
-			if (mainCharacter[0].velocityX > 2) {
+			if (level.mainCharacter[0].velocityX > 2) {
 				cameraX = cameraX + 2 < (this.props.width / 2) ? cameraX + 2 : cameraX
-			} else if (mainCharacter[0].velocityX < -2) {
-				cameraX = cameraX - 2 > (-this.props.width / 2) + mainCharacter[0].width ? cameraX - 2 : cameraX
+			} else if (level.mainCharacter[0].velocityX < -2) {
+				cameraX = cameraX - 2 > (-this.props.width / 2) + level.mainCharacter[0].width ? cameraX - 2 : cameraX
 			} else {
 				cameraX *= 0.98
 			}
@@ -204,7 +144,7 @@ export default class Game extends Component<Props> {
 			''
 
 		// $FlowFixMe
-		document.getElementById('mainCharacter').innerHTML = JSON.stringify(mainCharacter[0], undefined, 4)
+		document.getElementById('mainCharacter').innerHTML = JSON.stringify(level.mainCharacter[0], undefined, 4)
 	}
 
 	game = () => {
@@ -228,42 +168,42 @@ export default class Game extends Component<Props> {
 			}
 			if (isLeftPressed(keys, gamePad)) {
 				// walk left
-				mainCharacter[0].velocityX = -mainCharacter[0].maxVelocityX
+				level.mainCharacter[0].velocityX = -level.mainCharacter[0].maxVelocityX
 			} else if (isRightPressed(keys, gamePad)) {
 				// walk right
-				mainCharacter[0].velocityX = mainCharacter[0].maxVelocityX
-			} else if (mainCharacter[0].velocityX < 0.1 && mainCharacter[0].velocityX > -0.1) {
-				mainCharacter[0].velocityX = 0
+				level.mainCharacter[0].velocityX = level.mainCharacter[0].maxVelocityX
+			} else if (level.mainCharacter[0].velocityX < 0.1 && level.mainCharacter[0].velocityX > -0.1) {
+				level.mainCharacter[0].velocityX = 0
 			} else {
-				mainCharacter[0].velocityX *= 0.8
+				level.mainCharacter[0].velocityX *= 0.8
 			}
 			if (isJumpPressed(keys, gamePad)) {
 				// jump
 
-				if (mainCharacter[0].isStanding) {
-					mainCharacter[0].accelerationY = mainCharacter[0].maxAccelerationY
-					mainCharacter[0].velocityY = -mainCharacter[0].maxVelocityY
-					mainCharacter[0].isStanding = false
+				if (level.mainCharacter[0].isStanding) {
+					level.mainCharacter[0].accelerationY = level.mainCharacter[0].maxAccelerationY
+					level.mainCharacter[0].velocityY = -level.mainCharacter[0].maxVelocityY
+					level.mainCharacter[0].isStanding = false
 				}
 			}
 			if (isRunPressed(keys, gamePad)) {
 				// run
-				mainCharacter[0].maxVelocityX = 6
+				level.mainCharacter[0].maxVelocityX = 6
 			} else {
-				mainCharacter[0].maxVelocityX = 3
+				level.mainCharacter[0].maxVelocityX = 3
 			}
 
 			tick++
 
 			const { x, y, width, height } = this.getCameraBounds()
 
-			backgroundEntities.concat(
-				indestructibleEntities,
-				destructibleEntities,
-				enemyEntities,
-				effectEntities,
-				mainCharacter,
-				foreGroundEntities,
+			level.backgroundEntities.concat(
+				level.indestructibleEntities,
+				level.destructibleEntities,
+				level.enemyEntities,
+				level.effectEntities,
+				level.mainCharacter,
+				level.foreGroundEntities,
 			)
 				.filter(entity => isInsideBounds(
 					entity,
@@ -308,13 +248,13 @@ export default class Game extends Component<Props> {
 			}
 			const { x, y, width, height } = this.getCameraBounds();
 			[
-				[this.canvasBackgroundContext, backgroundEntities],
-				[this.canvasIndestructiblesContext, indestructibleEntities],
-				[this.canvasDestructiblesContext, destructibleEntities],
-				[this.canvasEnemiesContext, enemyEntities],
-				[this.canvasMainCharContext, mainCharacter],
-				[this.canvasEffectsContext, effectEntities],
-				[this.canvasForegroundContext, foreGroundEntities],
+				[this.canvasBackgroundContext, level.backgroundEntities],
+				[this.canvasIndestructiblesContext, level.indestructibleEntities],
+				[this.canvasDestructiblesContext, level.destructibleEntities],
+				[this.canvasEnemiesContext, level.enemyEntities],
+				[this.canvasMainCharContext, level.mainCharacter],
+				[this.canvasEffectsContext, level.effectEntities],
+				[this.canvasForegroundContext, level.foreGroundEntities],
 				[this.canvasMenuContext, menuEntities],
 			].forEach(([context, entities]) => {
 				if (context) {
