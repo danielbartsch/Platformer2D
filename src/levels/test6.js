989007fc33af93dd73bff5083e323cf6f8e157cf
danielbartsch <daniel.bartsch@live.at>
2017-12-22 21:48:15 +0100

    added test levels to iterate over

---
+++ src/levels/test6.js
@@ -0,0 +1,45 @@
+// @flow
+
+import { cloneDeep } from 'lodash'
+import type { Entity } from '../flowTypes'
+import * as EntityTypes from '../entityTypes'
+import { getDimensions } from '../entityUtils'
+
+export default (): { [entityNames: string]: Array<Entity> } => cloneDeep({
+	backgroundEntities: [],
+	indestructibleEntities: [
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -330, y: -260, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -285, y: -200, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -225, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -115, y: -360, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -45, y: -146, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -45, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: -73, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 90, y: -73, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 115, y: -360, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 180, y: -73, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 225, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+	],
+	destructibleEntities: [],
+	enemyEntities: [],
+	mainCharacter: [
+		{
+			type: EntityTypes.MAIN_CHARACTER,
+			...getDimensions(EntityTypes.MAIN_CHARACTER),
+			x: 0,
+			y: -200,
+			velocityX: 0,
+			velocityY: 0,
+			accelerationX: 0,
+			accelerationY: 0.9,
+			maxVelocityX: 3,
+			maxVelocityY: 24,
+			maxAccelerationX: 0.2,
+			maxAccelerationY: 0.9,
+			isStanding: false,
+			isObstacle: true,
+		},
+	],
+	effectEntities: [],
+	foreGroundEntities: [],
+})
---
+++ src/levels/test8.js
@@ -0,0 +1,38 @@
+// @flow
+
+import { cloneDeep } from 'lodash'
+import type { Entity } from '../flowTypes'
+import * as EntityTypes from '../entityTypes'
+import { getDimensions } from '../entityUtils'
+
+export default (): { [entityNames: string]: Array<Entity> } => cloneDeep({
+	backgroundEntities: [],
+	indestructibleEntities: [
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -285, y: -200, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -225, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: -45, y: 0, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+		{ type: EntityTypes.PLATFORM, ...getDimensions(EntityTypes.PLATFORM), isObstacle: true, x: 0, y: -73, accelerationX: 0, accelerationY: 0, maxAccelerationX: 0, maxAccelerationY: 0, maxVelocityX: 0, maxVelocityY: 0, velocityX: 0, velocityY: 0, isStanding: false },
+	],
+	destructibleEntities: [],
+	enemyEntities: [],
+	mainCharacter: [
+		{
+			type: EntityTypes.MAIN_CHARACTER,
+			...getDimensions(EntityTypes.MAIN_CHARACTER),
+			x: 0,
+			y: -200,
+			velocityX: 0,
+			velocityY: 0,
+			accelerationX: 0,
+			accelerationY: 0.9,
+			maxVelocityX: 3,
+			maxVelocityY: 28,
+			maxAccelerationX: 0.2,
+			maxAccelerationY: 0.9,
+			isStanding: false,
+			isObstacle: true,
+		},
+	],
+	effectEntities: [],
+	foreGroundEntities: [],
+})
